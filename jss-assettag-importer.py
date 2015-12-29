e3f6aa97eb1e1a9ea56f913cc87c5ee0b9bf437c
Brad Schmidt <bradschm@users.noreply.github.com>
2015-12-29 12:02:07 -0600

    Update jss-assettag-importer.py

--- jss-assettag-importer.py
+++ jss-assettag-importer.py
@@ -1,5 +1,6 @@
 #####################################################
 # JSS Asset Tag importer - v.2
+# Import Asset tags from a csv into your Casper JSS
 # Authored by Brad Schmidt @bradschm on 12/29/2015
 #####################################################
 
@@ -7,6 +8,31 @@
 # DISCLAIMER
 # I am not providing any kind of warranty. This has been thoroughly tested in my environments but I cannot guarantee that this script is not without bugs.
 # Thank you
+#####################################################
+
+#####################################################
+# LICENSE 
+# The MIT License (MIT)
+# 
+# Copyright (c) 2015 Brad Schmidt
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #####################################################
 
 #####################################################
